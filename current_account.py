bae080aa530a9b9be57669785ee2c32a25865703
Tuno-create <benbweseh@gmail.com>
2025-06-14 14:09:40 +0100

    added error and success messages

--- current_account.py
+++ current_account.py
@@ -3,3 +3,10 @@ from account import Account
 class CurrentAccount(Account):
     def __init__(self, balance):
         Account.__init__(self, balance)
+    
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print(f'Error: Insuffiecient funds')
+        else:
+            super().withdraw(amount)
+            print(f'Success: You have withdrawn {amount}. New balance is {self.balance}.')
--- savings_acccount.py
+++ savings_acccount.py
@@ -6,5 +6,10 @@ class SavingsAccount(Account):
         self.limit = limit
 
     def withdraw(self, amount):
-        if amount < self.limit:
+        if amount > self.limit:
+            print(f'Error: Cannot withdraw more than the limit of {self.limit}.')
+        elif amount > self.balance:
+            print('Error: Insufficient balance.')
+        else:
             super().withdraw(amount)
+            print(f'Withdrew {amount}. New balance: {self.balance}.')
